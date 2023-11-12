d8928e17beeadd7c87eb8f5e04b7baf5bc3e17f4
lerodeur666 <axelpichon06@gmail.com>
2023-11-12 11:02:54 +0100

    first

---
+++ bibliothèque.py
@@ -0,0 +1,38 @@
+
+#nom = [ "Thalador", "Dracaris", "Elowen", "Zephyrion", "Galadria", "Lythandra", "Thundrak", "Arvandus", "Sylveria" ]
+
+nom = ["Thalador", "Dracaris", "Elowen", "Zephyrion", "Galadria", "Lythandra", "Thundrak", "Arvandus", "Sylveria", "Solanar","Valerion", "Elyndra", "Falorian", "Nithralis", "Seraphina", "Caladrius", "Marwen", "Azurian", "Tyllaria", "Fenrisar","Elaris", "Althorin", "Thessalya", "Malachor", "Astridelle", "Orionis", "Selenea", "Bryston", "Eowyn", "Vaeloria","Sorenthor", "Valyndra", "Aeliana", "Galandriel", "Thundorin", "Eilinora", "Arion", "Alveron", "Isolde", "Drakonar","Thalira", "Elarion", "Ravendark", "Silveria", "Seraphiel", "Calandria", "Valandor", "Eldrith", "Arvandor", "Elysara","Thalindra", "Nyxarian", "Thalorin", "Aeloria", "Galadran", "Elariana", "Marvandor", "Thalara", "Elowynne", "Zephyros","Velorian", "Valandria", "Lirael", "Sylvaris", "Seraphina", "Selendria", "Thalador", "Elorin", "Aelarion", "Caladriel","Elaris", "Nethrion", "Tyllarian", "Thandorin", "Arvandria", "Galandor", "Elysium", "Vaelorian", "Valerian","Astridelle", "Lythorin", "Isolde", "Drakaris", "Sylveran", "Thalina", "Valeria", "Eloweth", "Selenea", "Malachar","Thundoril", "Alverian", "Eilinor", "Althorin", "Seraphiel", "Elysara", "Thessalya", "Elarion", "Vaeloria", "Sorenthor","Galadria"]
+
+occupation = ["vidangeur de latrines", "autre chose","serrurier","bedeau","joalier"]
+
+classe = ["clerc", "guerrier", "mage", "archer"]
+
+titre = "XXX"
+alignement = ["neutre","loyale","chaotique"]
+#vitesse (nombre de pas) = []
+px = "0"
+classe_armure = []
+points_de_vie = []
+force = []
+#modif 
+#agiliter + modif = []
+#endurance + modif = []
+#présence + modif = []
+#chance + modif = []
+#inteligence + modif = []
+#JDS réf = []
+#JDS vig = []
+#JDS vol = []
+#chanceux avec = []
+langages = ["le commun","nain","elfe"]
+#portrait ou symbole = []
+initiative = []
+dé_action = []
+attaque = []
+#dé critique = []
+#table critique = []
+armes = ["une truelle (dague)","une dague","un baton"]
+trésors = []
+armure = []
+équipement = []
+notes = []
